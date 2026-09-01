21af7db87fc0af07792aa54ff3b7ab7bf6cb3e4d
Rbmsoft <rbmsoft@Rbmsofts-MacBook-Air-2.local>
2026-09-01 13:43:15 +0530

    TICKET-83: fix commitlint config

--- Day 2/commitlint.config.js
+++ Day 2/commitlint.config.js
@@ -1,18 +1,19 @@
 module.exports = {
-  extends: ["@commitlint/config-conventional"],
+  //extends: ["@commitlint/config-conventional"],
   plugins: [
     {
         rules: {
             "header-match-pattern": ({ header }) => {
                 const pattern = /^TICKET-\d+: .+/;
                 const valid = pattern.test(header);
+
                 return[
                     valid, "commit message must match: TICKET-<number>: message",
                 ];
             },
         },
     },
-    ],
+ ],
   rules: {
     "header-match-pattern": [2, "always"],
   },
