e9d18a22657c2a4f566ab8c3480124a11cb013df
Rbmsoft <rbmsoft@Rbmsofts-MacBook-Air-2.local>
2026-09-01 14:21:33 +0530

    TICKET-83: fix formatting

--- Day 2/commitlint.config.js
+++ Day 2/commitlint.config.js
@@ -2,18 +2,16 @@ module.exports = {
   //extends: ["@commitlint/config-conventional"],
   plugins: [
     {
-        rules: {
-            "header-match-pattern": ({ header }) => {
-                const pattern = /^TICKET-\d+: .+/;
-                const valid = pattern.test(header);
+      rules: {
+        "header-match-pattern": ({ header }) => {
+          const pattern = /^TICKET-\d+: .+/;
+          const valid = pattern.test(header);
 
-                return[
-                    valid, "commit message must match: TICKET-<number>: message",
-                ];
-            },
+          return [valid, "commit message must match: TICKET-<number>: message"];
         },
+      },
     },
- ],
+  ],
   rules: {
     "header-match-pattern": [2, "always"],
   },
